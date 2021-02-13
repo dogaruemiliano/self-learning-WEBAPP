8a939e199d81cfc5731be3d1eb4ea78553f31d9f
dogaruemiliano <dogaru.emiliano@gmail.com>
2021-02-13 13:24:39 +0200

    Added validations and relations to models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :test
+
+  validates :test, presence: true
+  validates :question, presence: true
+  validates :correct_answer, presence: true
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :user
+  has_many :questions, dependent: :destroy
+
+  validates :user, presence: true
+  validates :name, presence: true
 end
