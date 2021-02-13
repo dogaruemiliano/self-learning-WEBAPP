33861f29b71688dd54e9757600c41bb45abec3c9
dogaruemiliano <dogaru.emiliano@gmail.com>
2021-02-13 13:37:26 +0200

    Added api/v1/messages controller with inedx and show

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,17 @@
-class Api::V1::MessagesController < ApplicationController
+class Api::V1::TestsController < Api::V1::BaseController
+  before_action :set_test, only: [ :show ]
+
   def index
+    @tests = policy_scope(Test)
   end
 
-  def create
+  def show
+  end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:id])
+    authorize @test
   end
 end
